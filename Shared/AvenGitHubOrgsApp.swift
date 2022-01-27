67abf51971334253890a9627ab8f26e0cd9339ad
curtiswilcox <curtiswilcox@brandeis.edu>
2022-01-27 14:43:44 -0500

    Initial Commit

---
+++ Shared/AvenGitHubOrgsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvenGitHubOrgsApp.swift
+//  Shared
+//
+//  Created by Curtis Wilcox on 1/27/22.
+//
+
+import SwiftUI
+
+@main
+struct AvenGitHubOrgsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
